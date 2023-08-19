1ab26e828472d983a2cbfaa57238f863d971fe90
NurlanNuruzada <nurlangn@code.edu.az>
2023-08-20 03:57:58 +0400

    Slider Added

--- FrontEnd/echooling/src/Components/AboutUsSection/AboutSecion.jsx
+++ FrontEnd/echooling/src/Components/AboutUsSection/AboutSecion.jsx
@@ -15,47 +15,65 @@ const AboutSecion = () => {
     navigate("/about");
   };
   return (
-    <div className={Styles.MainContainer}>
-      <div className={Styles.LeftSideContainer}>
-        <div className={Styles.BackgroundVideoContainer}>
-          <video className={Styles.BackgroundVideo} autoPlay muted loop>
-            <source src={BackgoundVideo} type="video/mp4" />
-          </video>
-        </div>
-        <img className={Styles.bakcgoundItem1} src={BackgroundImage1} alt="" />
-        <img className={Styles.bakcgoundItem2} src={BackgroundImage4} alt="" />
-        <img className={Styles.bakcgoundItem3} src={BackgroundImage2} alt="" />
-        <img className={Styles.bakcgoundItem4} src={BackgroundImage3} alt="" />
-      </div>
-      <div className={Styles.rightSideContainer}>
-        <h1 className={Styles.title1}>One of The Largest,</h1>
-        <h1 className={Styles.title2}>Most Online Course</h1>
-        <p className={Styles.title3}>
-          Why I say old chap that is spiffing in my flat such a fibber mufty
-          mush, porkies barney pukka only a quid a what a load of rubbish good
-          time.
-        </p>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <div style={{ display: "flex" }}>
-            <div>
-              <div className={Styles.Check}>
-                <FontAwesomeIcon icon={faCheck} />
-              </div>
-            </div>
-            <span>Access more then 100K online courses</span>
+    <div className={Styles.MainContainer1}>
+      <div className={Styles.MainContainer}>
+        <div className={Styles.LeftSideContainer}>
+          <div className={Styles.BackgroundVideoContainer}>
+            <video className={Styles.BackgroundVideo} autoPlay muted loop>
+              <source src={BackgoundVideo} type="video/mp4" />
+            </video>
           </div>
-          <div style={{ display: "flex" }}>
-            <div>
-              <div className={Styles.Check}>
-                <FontAwesomeIcon icon={faCheck} />
-              </div>
-            </div>
-            <span>Upskill your organization.</span>
-          </div>
+          <img
+            className={Styles.bakcgoundItem1}
+            src={BackgroundImage1}
+            alt=""
+          />
+          <img
+            className={Styles.bakcgoundItem2}
+            src={BackgroundImage4}
+            alt=""
+          />
+          <img
+            className={Styles.bakcgoundItem3}
+            src={BackgroundImage2}
+            alt=""
+          />
+          <img
+            className={Styles.bakcgoundItem4}
+            src={BackgroundImage3}
+            alt=""
+          />
         </div>
-        <Button onClick={handleGoAbout} color="#3270fc" mt={10}>
-          Read More
-        </Button>
+        <div className={Styles.rightSideContainer}>
+          <h1 className={Styles.title1}>One of The Largest,</h1>
+          <h1 className={Styles.title2}>Most Online Course</h1>
+          <p className={Styles.title3}>
+            Why I say old chap that is spiffing in my flat such a fibber mufty
+            mush, porkies barney pukka only a quid a what a load of rubbish good
+            time.
+          </p>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex" }}>
+              <div>
+                <div className={Styles.Check}>
+                  <FontAwesomeIcon icon={faCheck} />
+                </div>
+              </div>
+              <span>Access more then 100K online courses</span>
+            </div>
+            <div style={{ display: "flex" }}>
+              <div>
+                <div className={Styles.Check}>
+                  <FontAwesomeIcon icon={faCheck} />
+                </div>
+              </div>
+              <span>Upskill your organization.</span>
+            </div>
+          </div>
+          <Button onClick={handleGoAbout} color="#3270fc" mt={10}>
+            Read More
+          </Button>
+        </div>
       </div>
     </div>
   );
--- FrontEnd/echooling/src/Components/Slider/Slider.jsx
+++ FrontEnd/echooling/src/Components/Slider/Slider.jsx
@@ -1,18 +1,65 @@
 import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Styles from "./Slider.module.css";
-import image from "../../Images/Slider1.jpeg";
-import image2 from "../../Images/Slider2.jpeg";
+import Styles from "./Slider.module.css"; // Your custom styles
+import "slick-carousel/slick/slick.css"; // Main slick carousel CSS
+import "slick-carousel/slick/slick-theme.css"; // Slick theme CSS
+import image1 from "../../Images/Student1.png";
+import image2 from "../../Images/Student2.png";
+import image3 from "../../Images/Student3.png";
+import { Button } from "@chakra-ui/react";
+import { faChevronRight,faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const SliderCom = () => {
+  const sliderRef = React.useRef(null);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false, // Disable default arrows
+    pauseOnHover: true,
+    draggable: true,
+    slidesToShow: 1,
+  };
+
+  const handlePrevClick = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  const handleNextClick = () => {
+    sliderRef.current.slickNext();
+  };
 
   return (
-    <div className={Styles.MainContainer}>
-
+    <div className={Styles.SliderContainer}>
+      <Slider ref={sliderRef} {...settings} className={Styles.CustomSlider}>
+      <div>
+          <img className={Styles.customSlide} src={image1} alt="Slide 1" />
+        </div>
+        <div>
+          <img className={Styles.customSlide} src={image2} alt="Slide 2" />
+        </div>
+        <div>
+          <img className={Styles.customSlide} src={image3} alt="Slide 3" />
+        </div>
+      </Slider>
+      <div className={Styles.CustomButtonContainer}>
+        <Button className={Styles.CustomButton} onClick={handlePrevClick}>
+        <FontAwesomeIcon icon={faChevronLeft} style={{color: "#1a212e",}} />
+        </Button>
+        <Button className={Styles.CustomButton} onClick={handleNextClick}>
+        <FontAwesomeIcon icon={faChevronRight} style={{color: "#1a212e",}} />
+        </Button>
+      </div>
     </div>
   );
 };
 
 export default SliderCom;
+
+
+ 
