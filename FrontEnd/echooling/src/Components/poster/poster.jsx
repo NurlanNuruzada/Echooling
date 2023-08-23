60f1142bb7737fee866d377c98026835d760b6fa
NurlanNuruzada <nurlangn@code.edu.az>
2023-08-23 19:41:09 +0400

    evned left detail addded

---
+++ FrontEnd/echooling/src/Components/poster/poster.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Styles from "../poster/poster.module.css";
+const Poster = () => {
+  return (
+    <div className={Styles.Poster}>
+      <h1>Best Education theme</h1>
+    </div>
+  );
+};
+
+export default Poster;
--- FrontEnd/echooling/src/Pages/Eventdetail/EventDetails.jsx
+++ FrontEnd/echooling/src/Pages/Eventdetail/EventDetails.jsx
@@ -4,28 +4,86 @@ import {
   faTicket,
   faPerson,
   faArrowRight,
+  faUser,
+  faShareNodes,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-regular-svg-icons";
-import Button from "../../Components/Button/Button";
+import { Button, Center } from "@chakra-ui/react";
 import { Divider, Drawer, Flex } from "@chakra-ui/react";
+import Poster from "../../Components/poster/poster";
 const EventDetails = () => {
   return (
     <div className={Styles.MainContainer}>
       <div className={Styles.left}>
-        <div>
-          <Flex alignItems={"center"}>
-            <FontAwesomeIcon icon={faBookmark} style={{ color: "#000000" }} />
-            <p>Const:</p>
+        <div className={Styles.MainInfoContainer}>
+          <Flex
+            className={Styles.Icon}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Flex alignItems={"center"}>
+              <FontAwesomeIcon
+                className={Styles.Icon}
+                icon={faBookmark}
+                style={{ color: "#000000" }}
+              />
+              <p>Const :</p>
+            </Flex>
+            <h3>$72.00</h3>
           </Flex>
           <Divider />
-          <Flex alignItems={"center"}>
-            <FontAwesomeIcon icon={faPerson} />
-            <p>Total Slot:</p>
+          <Flex
+            className={Styles.Icon}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Flex alignItems={"center"}>
+              <FontAwesomeIcon className={Styles.Icon} icon={faUser} />
+              <p>organizer : </p>
+            </Flex>
+            <h4>Penny Tool</h4>
           </Flex>
           <Divider />
-         <Button>Join Now</Button>
+          <Flex
+            className={Styles.Icon}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Flex alignItems={"center"}>
+              <FontAwesomeIcon className={Styles.Icon} icon={faPerson} />
+              <p>Total Slot :</p>
+            </Flex>
+            <h4>250</h4>
+          </Flex>
+          <Divider />
+          <Button className={Styles.Button}>
+            Join Now!{" "}
+            <FontAwesomeIcon className={Styles.arrow} icon={faArrowRight} />
+          </Button>
+          <Flex
+            className={Styles.share}
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap={10}
+          >
+            <h5>Share this Event</h5>
+            <FontAwesomeIcon color="#3270fc" icon={faShareNodes} />
+          </Flex>
         </div>
+        <div className={Styles.DetatilsContainer}>
+          <h1>date:</h1>
+          <p>15 January, 2022 - December 14, 2022</p>
+          <h1>time:</h1>
+          <p>10:00 AM - 11:30PM</p>
+          <h1>loaction:</h1>
+          <p>IAC Building, New York</p>
+          <h1>Phone:</h1>
+          <p>+(624) 763 552 420</p>
+        </div>
+      </div>
+      <div className={Styles.DetatilsContainer}>
+        <Poster className={Styles.poster} />
       </div>
       <div className={Styles.right}></div>
     </div>
