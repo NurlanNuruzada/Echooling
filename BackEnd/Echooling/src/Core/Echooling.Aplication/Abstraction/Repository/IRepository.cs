88203d6cb7fd139c5112ad5eb6b835f0247045f2
NurlanNuruzada <nurlangn@code.edu.az>
2023-08-25 20:16:50 +0400

    added read Write Repositoryies

---
+++ BackEnd/Echooling/src/Core/Echooling.Aplication/Abstraction/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ecooling.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echooling.Aplication.Abstraction.Repository
+{
+    public interface IRepository<T> where T : BaseEntity, new()
+    {
+        public DbSet<T> Table { get; }
+
+    }
+}
