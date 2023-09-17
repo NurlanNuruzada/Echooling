301743e1ba074d16f93dfc7144b85306e3bd5077
nurlannuruzada <nurlangn@code.edu.az>
2023-09-17 05:17:24 +0400

    video_course Repositories Implemented

---
+++ BackEnd/Echooling/src/Infrastructure/Echooling.Persistance/Implementations/Repositories/VideoContent_CourseRepositories/VideoContent_CourseReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Echooling.Aplication.Abstraction.Repository.VideoContentCourseRepositories;
+using Echooling.Persistance.Contexts;
+using Ecooling.Domain.Entites;
+
+namespace Echooling.Persistance.Implementations.Repositories.VideoContent_CourseRepositories
+{
+    public class VideoContent_CourseReadRepository : ReadRepository<Video_Course>, IVideoContent_CourseReadRepository
+    {
+        public VideoContent_CourseReadRepository(AppDbContext context) : base(context) { }
+    }
+}
