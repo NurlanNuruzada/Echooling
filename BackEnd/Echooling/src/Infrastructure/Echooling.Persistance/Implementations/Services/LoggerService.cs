809908239d7f0cf67f0cada88abb90f1aa656d02
nurlannuruzada <nurlangn@code.edu.az>
2023-09-26 05:07:53 +0400

    added logger mapper Profile

--- BackEnd/Echooling/src/Infrastructure/Echooling.Persistance/Implementations/Services/LoggerService.cs
+++ BackEnd/Echooling/src/Infrastructure/Echooling.Persistance/Implementations/Services/LoggerService.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Echooling.Aplication.Abstraction.Repository.LoggerRepositories;
 using Echooling.Aplication.Abstraction.Services;
 using Echooling.Aplication.DTOs;
+using Echooling.Persistance.Resources;
 using Ecooling.Domain.Entites;
+using Microsoft.Extensions.Localization;
 
 namespace Echooling.Persistance.Implementations.Services
 {
     public class LoggerService : ILoggerService
     {
-        public Task CreateLog(CreateLogDto log)
+        private readonly ILoggerReadRepository _readRepository;
+        private readonly ILoggerWriteRepository _writeRepository;
+        private readonly IStringLocalizer<ErrorMessages> _localizer;
+        private readonly IMapper _mapper;
+
+        public LoggerService(ILoggerReadRepository readRepository,
+                             ILoggerWriteRepository writeRepository,
+                             IStringLocalizer<ErrorMessages> localizer,
+                             IMapper mapper)
         {
-            throw new NotImplementedException();
+            _readRepository = readRepository;
+            _writeRepository = writeRepository;
+            _localizer = localizer;
+            _mapper = mapper;
         }
 
+        public Task CreateLog(CreateLogDto log)
+        {
+            
+        }
         public Task<List<Logger>> getAllAsyc()
         {
             throw new NotImplementedException();
